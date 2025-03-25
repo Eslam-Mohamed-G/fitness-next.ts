6dd17971ba9a31632d1d6693c05d1d16df40f8ac
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-25 14:22:03 +0200

    added Home style

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function page() {
+export default function Home() {
     return (
-        <div>
-            Home
+        <div className='flex flex-row'>
+            <div className='grow border border-amber-900'>
+                Home
+            </div>
+            <div className='border border-amber-400 w-[50%]'>
+                <img src="/images/banner.png" alt="banner" className='w-full min-h-screen'/>
+            </div>
+            
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
+    <div className="min-h-screen px-4 gap-16 lg:ps-20 font-[family-name:var(--font-geist-sans)]">
+      <div className='flex flex-row h-[80vh] items-center pt-0 md:items-start md:pt-10 overflow-hidden'>
+        <div className='relative pt-0 md:pt-12 lg:ps-16'>
+          <p className="text-[red] text-[24px] font-bold mb-4 capitalize">fitness club</p>
+          <p className="text-black text-[44px] font-bold max-w-72">Sweat, Smile And Repeat</p>
+          <p className="z-50">Check out the most effective exercises personalized to you</p>
+          <button className="bg-[red] text-white capitalize rounded py-2 px-4 mt-4 cursor-pointer hover:bg-[#d00000] transition-all duration-300">
+            Explore exercises
+          </button>
+          <p className="uppercase text-rose-100 font-bold text-[200px] absolute -left-[3%] -bottom-[100%] hidden md:inline-block">exercises</p>
+        </div>
+
+        <div className="uppercase text-rose-100 font-bold text-6xl text-center absolute top-24 left-[80%] md:hidden z-0">
+          {"exercises".split("").map((letter, index) => (
+            <p key={index}>
+              {letter}
+            </p>
+          ))}
+        </div>
+
+        <div className='w-[38%] absolute top-0 right-5 lg:right-10 hidden md:block'>
+          <img src="/images/banner.png" alt="banner" className='w-full h-[65vh] lg:h-[90vh] -z-10' />
+        </div>
+      </div>
     </div>
   );
 }
